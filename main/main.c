e855ce9c7c0a302d38696da0f0ac1fd0f00ff0d4
Parth S Pawar <147683797+TaskMAster-p@users.noreply.github.com>
2026-07-12 17:06:19 +0530

    Clean up commented-out function declarations
    Removed commented-out function declarations to clean up code.

--- main/main.c
+++ main/main.c
@@ -13,21 +13,10 @@ static void nimble_host_task(void *param);
 
 // Library Prototypes
 void scan_init(void);
-// int gap_event(struct ble_gap_event *event, void *arg);
 
 void modbus_uart_init(void);
-// void process_modbus_request(uint8_t *rx, uint16_t len);
 void modbus_task(void *arg);
-// uint16_t modbus_crc(const uint8_t *buf, uint16_t len);
-
-// extern void print_hex(const uint8_t *data, uint8_t len);
-
-// extern int find_datoms_device(const uint8_t *mac);
-// extern void add_datoms_device(const uint8_t *mac,uint8_t slave_id);
-// int find_slave_index(uint8_t slave_id);
 int allocate_slave_slot(uint8_t slave_id);
-// void update_holding_registers(sensor_packet_t *pkt);
-// void print_holding_registers(void);
 
 
 static void on_stack_reset(int reason)
